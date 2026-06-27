965f807a0f522e3099129ec20b05a147784594e6
Alie <yangpai_beibei@163.com>
2026-06-28 01:11:37 +0800

    加入运行成功时的注释

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ response = client.chat.completions.create(
 print(response.choices[0].message.content)
 #本章要点：
 # 不同的apikey 使用的平台api也是不一样的，deepseek和anthropic 就完全不同
+
+# 依赖项：
+# 1.LLM ：申请与付费
+# 2.claude的安装-- CC Switch的安装 -- CC Switch接入claude
+# 3.python环境安装 （python，uv （是 Python 包管理工具））
