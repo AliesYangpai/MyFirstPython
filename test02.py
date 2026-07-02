90827e4a51231cc04b30157c1a7105ade89ddd64
Alie <yangpai_beibei@163.com>
2026-07-02 20:18:01 +0800

    python基础之 类创建，封装处理。

--- test02.py
+++ test02.py
@@ -14,6 +14,7 @@
 #
 # showInfo(name,age,gender)
 # showInfo(newName,newAge,newGender)
+from math import fabs
 
 # 3.数据类型，int，float，bool，complex ,字符串,占位符
 # num = 1
@@ -103,14 +104,104 @@
 
 # 6 字符串编码，字符串常见操作，列表，列表常见操作
 
-def do_print_str(str):
-    print("===do_print_str===star")
-    if not str:
-        print(" error str is invalidate")
-    else:
-        print(f"len:{len(str)}")
-        for i in str:
-            print(i)
-    print("===do_print_str===end")
-do_print_str("hello")
+# def do_print_str(str):
+#     print("===do_print_str===star")
+#     if not str:
+#         print(" error str is invalidate")
+#     else:
+#         print(f"len:{len(str)}")
+#         for i in str:
+#             print(i)
+#     print("===do_print_str===end")
+# do_print_str("hello")
 
+# 7 列表 列表名=[a,b,c] 元素类型可以不一样
+# list1 = [1, 44, 55, 1, 22]
+#
+#
+# def do_show_list(list1):
+#     for item in list1:
+#         print(f"currentValue:{item}")
+#     print(f"list1: {list1} type: {type(list1)}")
+#
+#
+# do_show_list(list1)
+#
+# list2 = [1, 2, 3, 4]
+
+#
+# def do_add_value(param, list):
+#     list.append(param)
+#     print(f"do_add_value param:{param}")
+#     return list
+#
+#
+# def do_del_value(param, list):
+#     flag = False
+#     if param in list:
+#         list.remove(param)
+#         flag = True
+#         print(f"list contains {param} do remove flag:{flag}")
+#     print(f"list not contains {param} do remove flag:{flag}")
+#     return flag
+#
+#
+# def do_update_value(param, i, list):
+#     flag = False
+#     if 0 <= i < len(list):
+#         list[i] = param
+#         flag = True
+#         print("update success")
+#     else:
+#         print("update error")
+#     return flag
+#
+# def do_fetch_list(list):
+#     print(f"list: {list}")
+#
+# def operate_work():
+#     list = [1,2,3,4,5]
+#     do_add_value(27, list)
+#     do_fetch_list(list)
+#
+#     do_del_value(5, list)
+#     do_fetch_list(list)
+#
+#     do_update_value(44,2,list)
+#     do_fetch_list(list)
+#
+#
+# operate_work()
+
+# 8 元组，字典，集合
+# def do_test_work():
+#     trp = (1,6,"name")
+#     list = [12,3,trp,5]
+#     e =list.index(trp)
+#     print(e)
+#     print(list)
+# do_test_work()
+#
+# def do_show_dic():
+#     dic = {"name":"tom","age":17,"gender":"male"}
+#     for key,value in dic.items():
+#         print(key,value)
+# do_show_dic()
+
+# 9 类型转换，深浅拷贝，可变对象，不可变对象
+# import copy
+# def do_work_copy():
+#     s1 = {1,66,69}
+#     di = {"name":"tome","age":24}
+#     list = [1,2,3,s1,di]
+#     new_list =copy.copy(list)
+#     print(f"list point:{id(list)} new_list point:{id(new_list)}")
+#     print(f"s1 point:{id(list[3])} new_list.s1 point:{id(new_list[3])}")
+#
+# do_work_copy()
+
+# 10 函数、返回、参数、函数嵌套
+def do_work_9(*args , param =6):
+    print(f"args:{args} param:{param}")
+
+do_work_9(1,2,3,9,0)
---
+++ test04.py
@@ -0,0 +1,78 @@
+# # class Student(object):
+# #     def __init__(self,name,age):
+# #         self.name=name
+# #         self.age=age
+#
+# class Student(object):
+#     def __init__(self,name,age,info):
+#         self.name=name
+#         self.age=age
+#         self.info=info
+#
+#     def do_eat(self):
+#         print(self.name+"do eat")
+#
+#     def do_study(self):
+#         print(self.name+"do study")
+#
+# student = Student("tom",12,"this is good student")
+# # student.do_eat()
+# # student.do_study()
+#
+#
+# class Animal:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def eat(self,food):
+#         print(self.name+" do eat "+food )
+#
+# dog = Animal("tom",1)
+# dog.eat("bound")
+
+# class Student:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def __del__(self):
+#         print("Student.__del__")
+#
+#     def show_info(self):
+#         print(f"Student.show_info name:${self.name} age:${self.age}")
+#
+# class Animal:
+#     def __init__(self):
+#         print("Animal.__init__")
+#     def __del__(self):
+#         print("Animal.__del__")
+#
+#     def show_info(self):
+#         print("Animal.show_info")
+# student = Student("Tom",12)
+# dog = Animal()
+# student.show_info()
+# dog.show_info()
+
+class Engin:
+    def __init__(self,name):
+        print("Engin__init__")
+        self.name=name
+    def __del__(self):
+        print("Engin__del__")
+
+    def do_start_work(self):
+        print(f"${self.name} start_work")
+
+class Car:
+    def __init__(self,engine):
+        print("Car__init__")
+        self.engine=engine
+    def __del__(self):
+        print("Car__del__")
+
+    def do_start_work(self):
+        print(" Car start_work")
+        self.engine.do_start_work()
+
+car=Car(Engin("v8"))
+car.do_start_work()
