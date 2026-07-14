62040f33019555df23297947b7eb58f6748a5003
Alie <yangpai_beibei@163.com>
2026-07-14 11:35:25 +0800

    work01.py list列表练习

--- black_work/work01.py
+++ black_work/work01.py
@@ -3,6 +3,11 @@
 # 03数据存储容器
 # 04函数
 # 05面向对象
+import statistics
+from audioop import avg
+
+from openai import OpenAI
+
 
 #####
 # 01数据存储于运算
@@ -85,3 +90,120 @@
 #             print("others~~~")
 #
 # condition_work5()
+
+# def loop_while_work():
+#     i = 0
+#     while i < 10:
+#         print("wo yao xue xi")
+#         i+=1
+# loop_while_work()
+
+# def loop_while_work2():
+#     num = 1
+#     sum = 0
+#     while num <= 100:
+#         if num %2 == 0:
+#             sum+=num
+#         num+=1
+#     print(sum)
+# loop_while_work2()
+
+# def loop_for_work3():
+#     tip = "hello-work"
+#     for e in tip:
+#         print(e)
+#
+# loop_for_work3()
+
+# def loop_for_work4():
+#     sum = 0
+#     for e in range(1,100):
+#         if e %2 !=0:
+#             sum+=e
+#
+#     print(sum)
+#
+# def loop_for_work5():
+#     sum = 0
+#     for e in range(100,500):
+#         if e %3 ==0:
+#             sum+=e
+#     print(sum)
+# loop_for_work4()
+# loop_for_work5()
+
+# def loop_all_work6():
+#     account1 = "admin"
+#     pass_word1 = "666888"
+#     account2 = "root"
+#     pass_word2 = "99999"
+#     account3 = "zhangsan"
+#     pass_word3 = "123456"
+#
+#     while True:
+#         enter_account = input("enter account: ")
+#         enter_password = input("enter password: ")
+#         if enter_account == account1 and enter_password == pass_word1:
+#             print(f"{enter_account} is working")
+#             break
+#         elif enter_account == account2 and enter_password == pass_word2:
+#             print(f"{enter_account} is working")
+#             break
+#         elif enter_account == account3 and enter_password == pass_word3:
+#             print(f"{enter_account} is working")
+#             break
+#         else:
+#             print(f"{enter_account} is not working,re_enter")
+# loop_all_work6()
+
+#####
+# 03数据存储容器
+# def collection_list_work1():
+#     students = ["tom","jerry","torch"]
+#     students.append("lorra")
+#     if 'jerry' in students:
+#         students.remove("jerry")
+#     for student in students:
+#         print(student)
+#
+# collection_list_work1()
+
+# def collection_list_work2():
+#     students = ["tom","jerry","torch","lorra","jim"]
+#     new_student = students[0:3] #切片，实际上就是截取数据
+#     print(id(students))
+#     print(id(new_student))
+#
+#     for e in new_student:
+#         print(e)
+#
+# collection_list_work2()
+
+# def collection_list_work3():
+#     my_list = []
+#     count = 0
+#     while count <10:
+#         num = int(input("请输出数字："))
+#         my_list.append(num)
+#         count += 1
+#     my_list.sort()
+#     print(f"max:{my_list[0]} min:{my_list[9]} avg:{statistics.mean(my_list)} avg:{sum(my_list)/len(my_list)}")
+#
+# collection_list_work3()
+
+# 去重处理
+def collection_list_work4():
+    my_list1 = [1,4,3,2,5]
+    my_list2 = [4,8,6,7,5]
+
+    # merge_list = my_list1 + my_list2
+    merge_list = [*my_list1, *my_list2]
+    my_new_list = []
+    for item in merge_list:
+        if item not in my_new_list:
+            my_new_list.append(item)
+
+    my_new_list.sort()
+    print(my_new_list)
+collection_list_work4()
+
