7383177b3d05b045a75ea874fec2b418b2f712b1
Alie <yangpai_beibei@163.com>
2026-07-15 22:16:51 +0800

    entity.py http_util.py main.py 新增异步.yaml文件读取写入逻辑

--- awesome_work/stage_00/http_util.py
+++ awesome_work/stage_00/http_util.py
@@ -1,8 +1,9 @@
 import asyncio
-from xxlimited_35 import Null
 
+import aiofiles
 import aiohttp
 import requests
+import yaml
 
 from awesome_work.stage_00.entity import HttpResponse
 
@@ -40,3 +41,64 @@ class HttpUtil:
 
     def fetch_async(self, url: str) -> HttpResponse:
         return asyncio.run(self.execute_fetch_async(url))
+
+    async def execute_open_file(self, file_path: str) -> None:
+        try:
+            async with aiofiles.open(file_path, 'r', encoding="utf-8") as f:
+                lines = await f.readlines()
+                for line in lines:
+                    print(line.strip())
+        except Exception as e:
+            print(f"execute_open_file error:{e}")
+
+    def open_file(self, file_path: str) -> None:
+        asyncio.run(self.execute_open_file(file_path))
+
+    async def execute_write_file(self, file_path, msg) -> None:
+        try:
+            async with aiofiles.open(file_path, 'w', encoding="utf-8") as f:
+                for index in range(0, 9):
+                    await f.write(msg + "\n")
+        except Exception as e:
+            print(f"execute_write_file error:{e}")
+
+    def write_file(self, file_path: str, msg: str) -> None:
+        asyncio.run(self.execute_write_file(file_path, msg))
+
+    async def execute_write_amend_file(self, file_path: str, msg: str) -> None:
+        try:
+            async with aiofiles.open(file_path, 'a', encoding="utf-8") as f:
+                for index in range(0, 4):
+                    await f.write(msg + "\n")
+        except Exception as e:
+            print(f"execute_write_amend_file error:{e}")
+
+    def write_amend_file(self, file_path: str, msg: str) -> None:
+        asyncio.run(self.execute_write_amend_file(file_path, msg))
+
+    async def execute_read_yaml_file(self, file_path: str) -> dict:
+        try:
+            async with aiofiles.open(file_path, 'r', encoding="utf-8") as f:
+                data = await f.read()
+                return yaml.safe_load(data)
+        except Exception as e:
+            print(f"execute_read_yaml_file error:{e}")
+            return None
+
+    def read_yaml_file(self, file_path: str) -> dict:
+        return asyncio.run(self.execute_read_yaml_file(file_path))
+
+    async def execute_write_yaml_file(self, file_path: str, dict: dict) -> None:
+        try:
+            async with aiofiles.open(file_path, 'r', encoding="utf-8") as f:
+                str_data = await f.read()
+                config_data = yaml.safe_load(str_data)
+            config_data |= dict
+            async with aiofiles.open(file_path, 'w', encoding="utf-8") as f:
+                await f.write(yaml.dump(config_data, allow_unicode=True, sort_keys=False, default_flow_style=False))
+
+        except Exception as e:
+            print(f"execute_write_yaml_file error:{e}")
+
+    def write_yaml_file(self, file_path: str, dict: dict) -> None:
+        asyncio.run(self.execute_write_yaml_file(file_path, dict))
--- awesome_work/stage_00/main.py
+++ awesome_work/stage_00/main.py
@@ -1,3 +1,5 @@
+from multipart import file_path
+
 from awesome_work.stage_00.http_util import HttpUtil
 
 url = "https://hacker-news.firebaseio.com/v0/item/31353677.json"
@@ -14,5 +16,24 @@ def start_request2():
 
 
 if __name__ == "__main__":
+    # 同步请求
     start_request()
-    # start_request2()
+    # 异步请求
+    start_request2()
+
+    # 异步读取文件
+    file_path = "./testfile.txt"
+    # http_util.open_file(file_path)
+    # 异步重写文件内容
+    # http_util.write_file(file_path,"好好学习，天天向上")
+    # 异步，追加文件内容
+    http_util.write_amend_file(file_path,"就那么回事儿了，哎～")
+    yaml_file_path = "./../../config.yaml"
+    config_file = http_util.read_yaml_file(yaml_file_path)
+    print(f"yaml: name:{config_file['name']}")
+
+    http_util.write_yaml_file(yaml_file_path,{"author":"albert","author_code":"001"})
+
+    # config_file["author"] = "albert"
+    # config_file["author_code"] = "001"
+    # http_util.write_yaml(file_path,config_file)
